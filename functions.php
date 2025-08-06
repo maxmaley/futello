ff177ae96360d8570a3abd14f4107e30f554178b
maxmaley <oksmaleyniks@gmail.com>
2025-08-06 17:08:33 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -83,3 +83,34 @@ function futello_excerpt_length($length) {
 }
 add_filter('excerpt_length', 'futello_excerpt_length');
 ?>
+
+/**
+ * Подключение стилей и скриптов
+ */
+function futello_enqueue_assets() {
+    // Google Fonts - Poppins с разными весами
+    wp_enqueue_style(
+        'futello-fonts',
+        'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap',
+        array(),
+        null
+    );
+    
+    // Основные стили темы
+    wp_enqueue_style(
+        'futello-style',
+        get_stylesheet_uri(),
+        array('futello-fonts'),
+        wp_get_theme()->get('Version')
+    );
+    
+    // JavaScript для интерактивности
+    wp_enqueue_script(
+        'futello-script',
+        get_template_directory_uri() . '/js/futello.js',
+        array(),
+        wp_get_theme()->get('Version'),
+        true
+    );
+}
+add_action('wp_enqueue_scripts', 'futello_enqueue_assets');
