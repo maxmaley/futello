c36daee48cb02322489d098caa2d58a9982341e7
maxmaley <oksmaleyniks@gmail.com>
2025-08-06 17:10:52 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -41,51 +41,7 @@ function futello_theme_setup() {
 add_action('after_setup_theme', 'futello_theme_setup');
 
 /**
- * Подключение стилей и скриптов
- */
-function futello_enqueue_assets() {
-    // Google Fonts - Poppins с разными весами
-    wp_enqueue_style(
-        'futello-fonts',
-        'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap',
-        array(),
-        null
-    );
-    
-    // Основные стили темы
-    wp_enqueue_style(
-        'futello-style',
-        get_stylesheet_uri(),
-        array('futello-fonts'),
-        wp_get_theme()->get('Version')
-    );
-}
-add_action('wp_enqueue_scripts', 'futello_enqueue_assets');
-
-/**
- * Fallback меню если не создано основное меню
- */
-function futello_fallback_menu() {
-    echo '<ul class="nav-menu">';
-    echo '<li><a href="' . home_url() . '">Home</a></li>';
-    echo '<li><a href="' . home_url('/predictions') . '">Predictions</a></li>';
-    echo '<li><a href="' . home_url('/reviews') . '">Reviews</a></li>';
-    echo '<li><a href="' . home_url('/statistics') . '">Statistics</a></li>';
-    echo '<li><a href="' . home_url('/about') . '">About us</a></li>';
-    echo '</ul>';
-}
-
-/**
- * Настройка длины отрывка
- */
-function futello_excerpt_length($length) {
-    return 20;
-}
-add_filter('excerpt_length', 'futello_excerpt_length');
-?>
-
-/**
- * Подключение стилей и скриптов
+ * Подключение стилей и скриптов (ИСПРАВЛЕННАЯ ВЕРСИЯ)
  */
 function futello_enqueue_assets() {
     // Google Fonts - Poppins с разными весами
@@ -114,3 +70,25 @@ function futello_enqueue_assets() {
     );
 }
 add_action('wp_enqueue_scripts', 'futello_enqueue_assets');
+
+/**
+ * Fallback меню если не создано основное меню
+ */
+function futello_fallback_menu() {
+    echo '<ul class="nav-menu">';
+    echo '<li><a href="' . home_url() . '">Home</a></li>';
+    echo '<li><a href="' . home_url('/predictions') . '">Predictions</a></li>';
+    echo '<li><a href="' . home_url('/reviews') . '">Reviews</a></li>';
+    echo '<li><a href="' . home_url('/statistics') . '">Statistics</a></li>';
+    echo '<li><a href="' . home_url('/about') . '">About us</a></li>';
+    echo '</ul>';
+}
+
+/**
+ * Настройка длины отрывка
+ */
+function futello_excerpt_length($length) {
+    return 20;
+}
+add_filter('excerpt_length', 'futello_excerpt_length');
+?>
